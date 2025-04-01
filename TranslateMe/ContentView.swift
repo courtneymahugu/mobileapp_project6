48f393eee3b1cd4a64eb785f64cb9b802c3dce14
courtneymahugu <courtneymahugu@gmail.com>
2025-04-01 16:39:25 -0400

    Initial Commit

---
+++ TranslateMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranslateMe
+//
+//  Created by Courtney Mahugu on 4/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranslateMe/TranslateMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeApp.swift
+//  TranslateMe
+//
+//  Created by Courtney Mahugu on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
